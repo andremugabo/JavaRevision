9a2f3d47fe58b9bae1f276275929ecc7798276e6
Mugabo Andre <andremugabo@yahoo.fr>
2025-03-04 09:37:24 +0200

    frame

---
+++ HELLOWORLD/SwingTest.java
@@ -0,0 +1,12 @@
+package HELLOWORLD;
+
+import java.awt.JFrame;;
+
+public class SwingTest {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Swing Test");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
